f46cad757c314ae4609ef9b8cc91cd6d3e88f90a
Casey <casey@scribd.com>
2022-01-10 11:36:18 -0600

    use spaces instead of tabs

--- dist/index.js
+++ dist/index.js
@@ -3859,31 +3859,32 @@ const core = __nccwpck_require__(186)
 const glob = __nccwpck_require__(90)
 
 async function run() {
-	try {
-		// set the AWS env variables
-		process.env.AWS_ACCESS_KEY_ID = core.getInput('access-key-id')
-		process.env.AWS_SECRET_ACCESS_KEY = core.getInput('secret-access-key')
-		process.env.AWS_DEFAULT_REGION = core.getInput('region')
+  try {
+    // set the AWS env variables
+    process.env.AWS_ACCESS_KEY_ID = core.getInput('access-key-id')
+    process.env.AWS_SECRET_ACCESS_KEY = core.getInput('secret-access-key')
+    process.env.AWS_DEFAULT_REGION = core.getInput('region')
 
-		// find the first matching file
-		const globber = await glob.create(core.getInput('path'))
-		const files = await globber.glob()
+    // find the first matching file
+    const globber = await glob.create(core.getInput('path'))
+    const files = await globber.glob()
 
-		// confirm a file was found
-		if (files.length <= 0) {
-			throw `No files matching the pattern '${core.getInput('path')}' found. Exiting.`
-		}
+    // confirm a file was found
+    if (files.length <= 0) {
+      throw `No files matching the pattern '${core.getInput('path')}' found. Exiting.`
+    }
 
-		// upload the file
-		const path = files[0]
-		const destinationPath = core.getInput('destination') || path
-		await exec.exec('aws', ['s3', 'cp', path, `s3://${core.getInput('bucket')}/${destinationPath}`, '--region', core.getInput('region'), '--acl', core.getInput('permissions'), '--no-progress'])
-	} catch (error) {
-		core.setFailed(error)
-	}
+    // upload the file
+    const path = files[0]
+    const destinationPath = core.getInput('destination') || path
+    await exec.exec('aws', ['s3', 'cp', path, `s3://${core.getInput('bucket')}/${destinationPath}`, '--region', core.getInput('region'), '--acl', core.getInput('permissions'), '--no-progress'])
+  } catch (error) {
+    core.setFailed(error)
+  }
 }
 
 run()
+
 })();
 
 module.exports = __webpack_exports__;
